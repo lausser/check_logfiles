6ff93ee1f4fc8deb32bd6a704b73a3ab7be7820e
Gerhard Lausser <gerhard.lausser@consol.de>
2015-03-22 22:16:12 +0100

    read crimson logs with wevtutil

---
+++ plugins-scripts/Nagios/CheckLogfiles/Search/Wevtutil.pm
@@ -0,0 +1,3 @@
+Nach zwei WOchen Schnauze voll vom Crimson-API für Win32::Eventlog. Geht auch einfacher:
+https://social.technet.microsoft.com/Forums/windows/en-US/6f158957-28ea-4ce9-a688-ccfa7bbd16bd/wevtutil-command-options-for-date
+
